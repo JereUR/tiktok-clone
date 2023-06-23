f73c0ced962a04fbda673e636c4222b22e971dba
JereUR <jeremias.jdv@gmail.com>
2023-06-22 21:01:49 -0300

    feat(Root): Use new icons

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,18 +1,28 @@
 import styles from './styles.module.css'
 
 import { Heart } from '../Icons/Heart'
+import { Comment } from '../Icons/Comment'
+import { Share } from '../Icons/Share'
 
-export default function VideoPlayerActions() {
+export default function VideoPlayerActions({
+  likes = 3567,
+  comments = 132,
+  shares = 465,
+  hearted = false
+}) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title="like">{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title="comments">{comments}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title="shares">{shares}</strong>
       </div>
     </aside>
   )
