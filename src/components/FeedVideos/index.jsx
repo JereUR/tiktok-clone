edfaf4871c18522b924d9588ba8685a2a286df2f
JereUR <jeremias.jdv@gmail.com>
2023-06-25 22:29:16 -0300

    Feat(Root): New video file added

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -10,7 +10,7 @@ const VIDEOS = [
     likes: 123,
     shares: 213,
     comments: 34,
-    song: 'Dusk Till Dawn(完整版) - 硬曲Music',
+    songTitle: 'Dusk Till Dawn(完整版) - 硬曲Music',
     albumCover:
       '//p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/bfa47750dbb34d31a21d8c479b6b9b7f~c5_200x200.jpeg',
     src: 'assets/videos/Ala ala ala 😼🤙🏿.mp4'
@@ -22,9 +22,22 @@ const VIDEOS = [
     likes: 198,
     shares: 2313,
     comments: 794,
-    song: 'Dusk Till Dawn(完整版) - 硬曲Music',
+    songTitle: 'La Kabra - The Farmlopez',
     albumCover:
-      '//p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/bfa47750dbb34d31a21d8c479b6b9b7f~c5_200x200.jpeg',
+      '//p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/488ba59d88d74de49ed9ecc977caf79f~c5_200x200.jpeg',
+    src: 'assets/videos/goat.mp4'
+  },
+  {
+    id: 3,
+    author: 'JereUR',
+    description:
+      'Demostración de como hacer un juego #GamerEnTikTok #GaminOnTikTok #GeekTok #Viewfinder #Tasher',
+    likes: 198,
+    shares: 2313,
+    comments: 794,
+    songTitle: ' Im Peppa Pig - Peppa Pig',
+    albumCover:
+      '//p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/fe5ae823f2cb4ebcab7e5432eaa605ee~c5_200x200.jpeg',
     src: 'assets/videos/goat.mp4'
   }
 ]
