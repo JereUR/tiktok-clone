203268f9dc50a2157790847188ffe11e6c22f54c
JereUR <jeremias.jdv@gmail.com>
2023-06-27 22:18:55 -0300

    feat(Root): Get videos in supabase database with username owner

---
+++ src/services/index.js
@@ -0,0 +1,12 @@
+import { supabase } from './supabase'
+
+export const getVideos = async () => {
+  const { data: videos, error } = await supabase.from('videos').select(`
+    *,
+    users (
+      username
+    )
+  `)
+
+  return [error, videos]
+}
