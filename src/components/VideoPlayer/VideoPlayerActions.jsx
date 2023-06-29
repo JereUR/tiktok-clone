20cf47c88869d824a374a9ee77e871303f33678b
JereUR <jeremias.jdv@gmail.com>
2023-06-28 21:52:05 -0300

    feta(Root): Added video user photo

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -8,7 +8,9 @@ export default function VideoPlayerActions({
   likes = 3567,
   comments = 132,
   shares = 465,
-  hearted = false
+  hearted = false,
+  username,
+  avatar
 }) {
   const handleLike = () => {
     alert('Like')
@@ -24,18 +26,28 @@ export default function VideoPlayerActions({
 
   return (
     <aside className={styles.actions}>
-      <div className={styles.action} onClick={handleLike}>
-        <Heart />
+      <div className={styles.user}>
+        <img alt={username} src={avatar} />
+        <img
+          src="https://sf16-scmcdn-va.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/test-2e6dd40439e72f09a8193e27cb3e0c51.svg"
+          width="24"
+        />
+      </div>
+
+      <button onClick={handleLike} className={styles.action}>
+        <Heart width="45" />
         <span title="like">{likes}</span>
-      </div>
-      <div className={styles.action} onClick={handleComment}>
-        <Comment />
+      </button>
+
+      <button onClick={handleComment} className={styles.action}>
+        <Comment width="45" />
         <span title="comments">{comments}</span>
-      </div>
-      <div className={styles.action} onClick={handleShare}>
-        <Share />
+      </button>
+
+      <button onClick={handleShare} className={styles.action}>
+        <Share width="45" />
         <span title="shares">{shares}</span>
-      </div>
+      </button>
     </aside>
   )
 }
