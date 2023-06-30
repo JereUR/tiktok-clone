2311606e3fa3b44b9a299bb83202ffe69d925af4
JereUR <jeremias.jdv@gmail.com>
2023-06-30 02:49:31 -0300

    feat(Root): Create and use intersection observer to play video when this is on window view

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,9 +1,10 @@
 import styles from './styles.module.css'
 import clsx from 'clsx'
 
-import { useState, useRef } from 'react'
+import { useRef } from 'react'
 import VideoPlayerActions from './VideoPlayerActions'
 import VideoDescription from '../VideoDescription'
+import useIntersectionVideoPlayer from '../../hooks/useIntersectionVideoPlayer'
 
 export default function VideoPlayer({
   albumCover,
@@ -13,16 +14,8 @@ export default function VideoPlayer({
   src,
   songTitle
 }) {
-  const [playing, setPlaying] = useState(false)
   const video = useRef(null)
-
-  const handlePlay = () => {
-    const { current: videoEl } = video
-    playing ? videoEl.pause() : videoEl.play()
-
-    setPlaying(!playing)
-  }
-
+  const { playing, handlePlay } = useIntersectionVideoPlayer({ video })
   const playerClassName = clsx(styles.player, { [styles.hidden]: playing })
 
   return (
