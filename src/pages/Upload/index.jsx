8c1ce8d46ecec25739bf69f178b8e4e303bff5be
JereUR <jeremias.jdv@gmail.com>
2023-07-01 01:59:41 -0300

    feat(Root): Drag and drop elemetn and styles for upload a video

--- src/pages/Upload/index.jsx
+++ src/pages/Upload/index.jsx
@@ -1,17 +1,45 @@
 import { useDropzone } from 'react-dropzone'
 import styles from './styles.module.css'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
+import clsx from 'clsx'
 
 export default function Upload() {
   const onDrop = (e) => {
     console.log('Drop', e)
   }
 
-  const { getRootProps, getInputProps } = useDropzone({
-    maxFiles: 1,
-    accept: 'video/mp4,video/x-m4v,video/*',
-    onDrop
+  const { isDragAccept, isDragReject, getRootProps, getInputProps } =
+    useDropzone({
+      maxFiles: 1,
+      accept: { 'video/mp4,video/x-m4v,video/*': [] },
+      onDrop
+    })
+
+  useEffect(() => {
+    if (isDragReject) navigator.vibrate(100)
+  }, [isDragReject])
+
+  const dndClassNames = clsx(styles.dnd, {
+    [styles.dndReject]: isDragReject,
+    [styles.dndAccept]: isDragAccept
   })
+
+  const renderDndContent = () => {
+    if (isDragReject) return <h4>Archivo no soportado</h4>
+    if (isDragAccept) return <h4>¡Suelta el archivo para subirlo!</h4>
+
+    return (
+      <>
+        <h4>Selecciona el video para cargar</h4>
+        <h5>O arrastra y suelta un archivo</h5>
+        <ul>
+          <li>MP4 o WebM</li>
+          <li>Resolución de al menos 720x1280</li>
+          <li>Hasta 180 segundos</li>
+        </ul>
+      </>
+    )
+  }
 
   return (
     <div className={styles.upload}>
@@ -21,21 +49,14 @@ export default function Upload() {
       <form className={styles.form}>
         <div {...getRootProps()}>
           <input {...getInputProps()} />
-          <div className={styles.dnd}>
+          <div className={dndClassNames}>
             <img
               src="https://sf16-scmcdn-va.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/cloud_icon-6e07be44878e69ee3f7bff3b78405b76.svg"
               width="49"
             />
-            <h4>Selecciona el video para cargar</h4>
-            <h5>O arrastra y suelta un archivo</h5>
-            <ul>
-              <li>MP4 o WebM</li>
-              <li>Resolución de al menos 720x1280</li>
-              <li>Hasta 180 segundos</li>
-            </ul>
+            {renderDndContent()}
           </div>
         </div>
-
         <label>
           leyenda
           <input type="text" placeholder="" />
